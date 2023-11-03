e95f28a06c671da9fd8238ed6d442a7c397713bd
Georgiy Garenov <g_garenov@outlook.com>
2023-11-03 19:01:11 +0200

    A raw timer component was added, but needs to be created edited and build.

--- components-basics/src/App.jsx
+++ components-basics/src/App.jsx
@@ -1,11 +1,14 @@
 import MovieList from './components/MovieList'
 import './App.css'
 import movies from './assets/movies';
+import Timer from './components/Timer';
 
 function App() {
   return (
     <div>
       <h1>My first Dynamic React App</h1>
+
+      <Timer></Timer>
 
       <MovieList movies={movies} headingText="Movie list"/>
       
