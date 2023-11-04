1033fa5d7a5cbd508c00ef746dd628fdce43ce68
Georgiy Garenov <g_garenov@outlook.com>
2023-11-04 10:54:26 +0200

    Timer was successfully created using useState function

--- components-basics/src/components/Timer.jsx
+++ components-basics/src/components/Timer.jsx
@@ -1,7 +1,22 @@
-export default function Timer(props) {
+import {useState} from "react";
+
+export default function Timer(props) { 
+    const [time, setTime] = useState(0);
+    
+
+    // let time = 0;
+console.log(`Current time is ${time}`)
+    setTimeout(() => {
+       setTime(time + 1);
+        console.log(`Current time is ${time}`);
+
+    }, 1000);
+
     return (
         <div>
             <h3>Timer</h3>
+
+            <p>{time }</p>
         </div>
     )
 }
