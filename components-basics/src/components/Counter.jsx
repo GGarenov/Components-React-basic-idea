0b5b76b314508430a0ed7f1e6cbd8e66f82c31dc
Georgiy Garenov <g_garenov@outlook.com>
2023-11-04 11:37:52 +0200

    Clear counter button was implemented

--- components-basics/src/components/Counter.jsx
+++ components-basics/src/components/Counter.jsx
@@ -7,12 +7,17 @@ export default function Counter(props) {
         setCount(count + 1);
     };
 
+    const clearCounterHandler = () => {
+        setCount(0);
+    }
+
     return (
         <div>
             <h1>Counter</h1>
 
             <p>Counter: {count }</p>
             <button onClick={incrementClickHandler}>+</button>
+            <button onClick={clearCounterHandler}>clear</button>
             <button onClick={()=> setCount (count - 1)}>-</button>
         </div>
     )
